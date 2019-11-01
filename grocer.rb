086093940d14a373c7a996af508117a50532a482
Parker Bacall <pbacall2@gmail.com>
2019-11-01 03:48:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -64,7 +64,9 @@ def apply_clearance(cart)
   #
 i = 0
 while i < cart.length do
-  if cart[i][:clearance] = true
+  if cart[i][:clearance]
+    cart[i][:price] = cart[i][:price] *0.20
+  end
   i += 1
 end
 
